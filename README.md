eb376df06116cf50af36080ff2017a2893ffad48
WafikSleim <wafik_sleim@outlook.com>
2023-01-22 23:37:31 +0200

    first commit (#1)

--- README.md
+++ README.md
@@ -2,12 +2,17 @@
 
 ## Table of Contents
 
-* [Instructions](#instructions)
+*  name is **Wafik Sleim**
+*  **email** is (wafiksleim@gmail.com).
+*
+* Html and css mostly from udacity's startup project, but I did some changes
+* use Dom to generate a navbar
+* smooth scroll with css
+* add a scroll to top button
+* add and remove active class when scrolling through the sections
 
-## Instructions
+## sources I used
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+* https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+* https://developer.mozilla.org/en-US/docs/Glossary/Viewport
+* **w3schools** and **medium**
--- js/app.js
+++ js/app.js
@@ -1,62 +1,146 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
+let sections = document.querySelectorAll ( 'section' );
+const navBar = document.getElementById ( 'navbar__list' );
+const Scroll = document.createDocumentFragment ();
+const Top = document.getElementById ( 'BtmTop' );
+const AddNew = document.getElementById ( 'AddSection' );
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
 
+// Helper viewport function
+function ViewPort ( element ) {
+    const rect = element.getBoundingClientRect ();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= ( window.innerHeight || document.documentElement.clientHeight ) &&
+        rect.right <= ( window.innerWidth || document.documentElement.clientWidth )
+    );
+}
 
+//Html All Code
+function HtmlNav(section) {
+    return `<a href="#${ section.id }" class="menu__link ${ section.dataset.nav }" data-nav="${ section.id }">${ section.dataset.nav }</a>`
+}
+
+function HtmlSection () {
+    return `<div class="landing__container">
+        <h2>Section ${ sections.length + 1 }</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non diam euismod, vestibulum nisl non, ultrices augue. Sed id lectus in nunc tincidunt ultrices. Donec euismod, nisl nec consectetur tincidunt, ligula dolor aliquam enim, nec ultrices nunc turpis eu nunc. Sed auctor, nisl vitae molestie ultrices, nisl lacus ultrices nulla, eget ultricies libero odio at sem. Suspendisse potenti. Donec auctor, dolor sit amet aliquam eleifend, augue libero aliquam nisl, sed tincidunt risus nunc nec nisl. Nulla facilisi. Sed sed massa euismod, ultrices massa sit amet, faucibus nunc. Sed a nunc auctor, aliquam nisl vitae, aliquam nunc. Sed auctor, nisl vitae molestie ultrices, nisl lacus ultrices nulla, eget ultricies libero odio at sem. Suspendisse potenti. Donec auctor, dolor sit amet aliquam eleifend, augue libero aliquam nisl, sed tincidunt risus nunc nec nisl. Nulla facilisi. Sed sed massa euismod, ultrices massa sit amet, faucibus nunc. Sed a nunc auctor, aliquam nisl vitae, aliquam nunc.</p>
+        </div>`
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
+function buildNav () {
+    sections.forEach ( section => {
+        const navItem = document.createElement ( 'li' );
+        navItem.innerHTML = HtmlNav(section);
+        Scroll.appendChild ( navItem );
+    } );
+    navBar.appendChild ( Scroll );
+    //scroll Nav
+    scrollNav ()
+    // nav bar active state on scroll and click
+    window.addEventListener ( 'scroll' , () => {
+        sections.forEach ( section => {
+            if ( ViewPort ( section ) ) {
+                const activeLink = document.querySelector ( `[data-nav=${ section.id }]` );
+                activeLink.classList.add ( 'BtnActive' );
+            } else {
+                const activeLink = document.querySelector ( `[data-nav=${ section.id }]` );
+                activeLink.classList.remove ( 'BtnActive' );
+            }
+        } );
+    } )
+}
+
+//scroll Nav
+function scrollNav () {
+    navBar.addEventListener ( 'click' , ( e ) => {
+        e.preventDefault ();
+        const section = document.getElementById ( e.target.dataset.nav );
+        section.scrollIntoView ( { behavior : "smooth" } );
+    } );
+}
+
+// Scroll to top button
+function scrollToTop () {
+    Top.addEventListener ( 'click' , () => {
+        window.scrollTo ( { top : 0 , behavior : "smooth" } );
+    } );
+    window.addEventListener ( 'scroll' , () => {
+        if ( window.scrollY > 300 ) {
+            Top.style.display = 'block';
+        } else {
+            Top.style.display = 'none';
+        }
+    } );
+}
+
+// Add new section to the page
+function addNewSection () {
+    AddNew.addEventListener ( 'click' , () => {
+        sections = document.querySelectorAll ( 'section' );
+        const newSection = document.createElement ( 'section' );
+        newSection.setAttribute ( 'data-nav' , 'Section ' + ( sections.length + 1 ) );
+        newSection.setAttribute ( 'id' , 'section' + ( sections.length + 1 ) );
+        newSection.innerHTML = HtmlSection();
+        document.querySelector ( 'main' ).appendChild ( newSection );
+        AddNavItem(newSection);
+    } );
+}
 
 
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
+//Add NavBar Item
+function AddNavItem(newSection) {
+    const navItem = document.createElement ( 'li' );
+    navItem.innerHTML = `<a href="#${ newSection.id }" class="menu__link ${ newSection.dataset.nav }" data-nav="${ newSection.id }">${ newSection.dataset.nav }</a>`;
+    navBar.appendChild ( navItem );
+}
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+// Build menu
+buildNav ();
+// Scroll to top button
+scrollToTop ();
+// Add new section
+addNewSection ();
